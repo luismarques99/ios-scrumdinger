ae4862f70e1803ec0b4471f0602189011a6ef433
Luis Marques <luisserafim99@hotmail.com>
2024-04-14 00:43:57 +0000

    Add trailing label style

---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,23 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Luís Marques on 14/04/2024.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self {
+        Self()
+    }
+}

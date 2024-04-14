4f1edd5e7500e9c487f49026dc82ba435ddc9e67
Luis Marques <luisserafim99@hotmail.com>
2024-04-14 00:44:08 +0000

    Add card view

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,38 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Luís Marques on 13/04/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+                .accessibilityAddTraits(.isHeader)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .labelStyle(.trailingIcon)
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+    }
+}
+
+#Preview {
+    var scrum = DailyScrum.sampleData[0]
+    return CardView(scrum: scrum)
+        .background(scrum.theme.mainColor)
+        .previewLayout(.fixed(width: 400, height: 60))
+}
